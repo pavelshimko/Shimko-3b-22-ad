609b84adca69c8434d1dab2ed7779d684d657bce
Pavel <pavel.shimko.nur@gmail.com>
2023-06-01 16:39:22 +0300

    упростил код для 45 таска

--- Task-45.py
+++ Task-45.py
@@ -2,14 +2,11 @@
 Имя существующего файла для проверки кода - тест
 """
 file_name = input()
-if file_name:
-    try:
-        with open(file_name, 'r') as file:
-            data = file.read()
-            print(data)
-    except FileNotFoundError:
-        print("Ошибка!")
-    except ValueError:
-        print("Ошибка!")
-else:
+try:
+    with open(file_name, 'r') as file:
+        data = file.read()
+        print(data)
+except FileNotFoundError:
+    print("Ошибка!")
+except Exception:
     print("Ошибка!")
