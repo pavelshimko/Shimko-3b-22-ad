f5e2571ac6311097252931c3be88e710b19f8e9d
Pavel <pavel.shimko.nur@gmail.com>
2023-06-03 14:00:54 +0300

    добавил исключение на случай не найденгог файла

--- Task-56.py
+++ Task-56.py
@@ -1,24 +1,26 @@
 """
 Чтобы попробовать функицонал используй файл test_2.txt
 """
+try:
+    file_name = input()
 
-file_name = input()
+    file = open(file_name, 'r', encoding='utf-8')
 
-file = open(file_name, 'r', encoding='utf-8')
+    content = file.read()
+    content_list = content.split()
 
-content = file.read()
-content_list = content.split()
+    unqiue_words_list = set(content_list)
+    max_mentions = 0
+    max_mentioned_word = None
 
-unqiue_words_list = set(content_list)
-max_mentions = 0
-max_mentioned_word = None
-
-for i in unqiue_words_list:
-    mentions = 0
-    for r in content_list:
-        if i == r:
-            mentions +=1
-    if mentions > max_mentions:
-        max_mentions = mentions
-        max_mentioned_word = i
-print(max_mentioned_word)
+    for i in unqiue_words_list:
+        mentions = 0
+        for r in content_list:
+            if i == r:
+                mentions +=1
+        if mentions > max_mentions:
+            max_mentions = mentions
+            max_mentioned_word = i
+    print(max_mentioned_word)
+except FileNotFoundError:
+    print("Ошибка")
