2873039f4e5e951281f34bd62baa08232c1b4bf1
Pavel <pavel.shimko.nur@gmail.com>
2023-07-10 14:50:15 +0300

    коммит оставшихся заданий

---
+++ Task-31.py
@@ -0,0 +1,16 @@
+class calculator():
+
+    def сложение(self, a, b):
+        return a + b
+
+    def вычитание(self, a, b):
+        return a - b
+
+    def умножение(self, a, b):
+        return a * b
+
+    def деление(self, a, b):
+        return a / b
+
+r = calculator()
+print(r.вычитание(10, 7))
